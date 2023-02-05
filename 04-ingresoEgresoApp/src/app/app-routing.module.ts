ed8fa8deed4fb91a88bf7dd9dfd03342fd41c8bb
zayaslui <zayaslui@gmail.com>
2023-02-05 17:42:49 -0300

    update

---
+++ 04-ingresoEgresoApp/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {path : 'login', component: LoginComponent},
+  {path : 'register', component: RegisterComponent},
+  {path : '', component: DashboardComponent},
+  {path : '**', redirectTo: ''},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
